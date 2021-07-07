ee5b57088e8e44ae5771c23e68a49e8091f2e7cf
Mario Jorge <mario.albuquerque@bancointer.com.br>
2021-07-07 18:44:56 -0300

    first commit

---
+++ src/main/java/TextFileWriter.java
@@ -0,0 +1,73 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class TextFileWriter {
+
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        try {
+
+            String excelFilePath = "/Users/mariojorgepaixaodealbuquerque/PJ/teste.xls";
+
+
+            FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+            Workbook workbook = new HSSFWorkbook(inputStream);
+            Sheet firstSheet = workbook.getSheetAt(0);
+            FileWriter file = new FileWriter("/Users/mariojorgepaixaodealbuquerque/PJ/fim.txt");
+            Iterator<Row> iterator = firstSheet.iterator();
+
+            while (iterator.hasNext()) {
+                Row nextRow = iterator.next();
+                Iterator<Cell> cellIterator = nextRow.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue());
+                            sb.append(cell.getStringCellValue() + ", ");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue());
+                            sb.append(String.valueOf(cell.getBooleanCellValue() + ", "));
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue());
+                            sb.append(String.valueOf(cell.getNumericCellValue() + ", "));
+                            break;
+                        case FORMULA:
+                            System.out.print(cell.getCellFormula());
+                            sb.append(String.valueOf(cell.getCellFormula() + ", "));
+                            break;
+                        default :
+                            System.out.print(cell.getStringCellValue());
+                            sb.append(cell.getStringCellValue() + ", ");
+                    }
+                    System.out.print(" - ");
+                }
+                System.out.println();
+                sb.append("\n");
+            }
+            workbook.close();
+            inputStream.close();
+            Path path = Paths.get("/Users/mariojorgepaixaodealbuquerque/PJ/fim.txt");
+            Files.write(path, Arrays.asList(sb.toString()));
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
